2a9e1ad7d0bd831ecdea6fea15f3cd24370d2627
Peridot <peridot491@protonmail.com>
2021-09-16 21:14:09 +0200

    Initial commit

---
+++ src/main/java/pl/chillcode/chillchat/ChillChat.java
@@ -0,0 +1,12 @@
+package pl.chillcode.chillchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChillChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
