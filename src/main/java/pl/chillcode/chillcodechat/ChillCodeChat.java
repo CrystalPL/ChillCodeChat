7e893c535488c23145c2276311e68c082e47606e
Crystal <janwolowski@crystalek.pl>
2021-11-04 21:18:43 +0100

    rewolucja

---
+++ src/main/java/pl/chillcode/chillcodechat/ChillCodeChat.java
@@ -0,0 +1,17 @@
+package pl.chillcode.chillcodechat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChillCodeChat extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
